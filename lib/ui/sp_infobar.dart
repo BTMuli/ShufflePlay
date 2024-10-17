8ae3eb9771550e1d99c05f217104485b7a6cc90d
目棃 <BT-Muli@outlook.com>
2024-10-17 19:07:33 +0800

    ♻️ infoBar 队列

--- lib/ui/sp_infobar.dart
+++ lib/ui/sp_infobar.dart
@@ -1,8 +1,66 @@
+// Dart imports:
+import 'dart:async';
+
 // Package imports:
 import 'package:fluent_ui/fluent_ui.dart';
 
 // Project imports:
 import '../models/bbs/bbs_base_model.dart';
+
+class SpInfoBarType {
+  /// info
+  InfoBar infoBar;
+
+  /// context
+  BuildContext context;
+
+  /// constructor
+  SpInfoBarType(this.infoBar, this.context);
+
+  /// show
+  Future<void> show() async {
+    await displayInfoBar(
+      context,
+      builder: (context, close) => infoBar,
+      duration: const Duration(seconds: 2),
+    );
+  }
+}
+
+/// 消息队列
+class SpInfobarQueue extends ChangeNotifier {
+  SpInfobarQueue._();
+
+  static final SpInfobarQueue _instance = SpInfobarQueue._();
+
+  factory SpInfobarQueue() => _instance;
+
+  final List<SpInfoBarType> _infoBars = [];
+
+  List<SpInfoBarType> get infoBars => _infoBars;
+
+  Timer? _timer;
+
+  Future<void> initTimer() async {
+    _timer = Timer.periodic(const Duration(milliseconds: 1500), (timer) async {
+      if (_infoBars.isNotEmpty) {
+        await _infoBars.first.show();
+        _infoBars.removeAt(0);
+        notifyListeners();
+      } else {
+        timer.cancel();
+      }
+    });
+  }
+
+  Future<void> add(SpInfoBarType infobar) async {
+    _infoBars.add(infobar);
+    if (_timer == null || !_timer!.isActive) {
+      await initTimer();
+    }
+    notifyListeners();
+  }
+}
 
 /// 对SnackBar的封装
 class SpInfobar {
@@ -14,15 +72,20 @@ class SpInfobar {
   /// 获取实例
   factory SpInfobar() => _instance;
 
+  /// 队列
+  static final _infoBarQueue = SpInfobarQueue();
+
   /// show
   static Future<void> show(
     BuildContext context,
     String text,
     InfoBarSeverity severity,
   ) async {
-    return await displayInfoBar(context, builder: (context, close) {
-      return InfoBar(title: Text(text), severity: severity);
-    });
+    var infoBar = SpInfoBarType(
+      InfoBar(title: Text(text), severity: severity),
+      context,
+    );
+    return await _infoBarQueue.add(infoBar);
   }
 
   /// bbs
