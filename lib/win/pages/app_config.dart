47cb61f2e531799e2ed91d14e960ba0f05510fb2
目棃 <BT-Muli@outlook.com>
2025-02-09 12:30:48 +0800

    ♻️ stateless

--- lib/win/pages/app_config.dart
+++ lib/win/pages/app_config.dart
@@ -1,9 +1,5 @@
-// Flutter imports:
-import 'package:flutter/foundation.dart';
-
 // Package imports:
 import 'package:fluent_ui/fluent_ui.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 // Project imports:
@@ -12,21 +8,11 @@ import '../widgets/app_config_game.dart';
 import '../widgets/app_config_info.dart';
 import '../widgets/app_config_user.dart';
 
-class AppConfigPage extends ConsumerStatefulWidget {
+class AppConfigPage extends StatelessWidget {
   const AppConfigPage({super.key});
 
   @override
-  ConsumerState<AppConfigPage> createState() => _AppConfigPageState();
-}
-
-class _AppConfigPageState extends ConsumerState<AppConfigPage>
-    with AutomaticKeepAliveClientMixin {
-  @override
-  bool get wantKeepAlive => true;
-
-  @override
   Widget build(BuildContext context) {
-    super.build(context);
     return ScaffoldPage(
       content: ListView(
         children: [
@@ -34,13 +20,11 @@ class _AppConfigPageState extends ConsumerState<AppConfigPage>
             padding: EdgeInsets.symmetric(horizontal: 16.w),
             child: const AppConfigInfoWidget(),
           ),
-          if (defaultTargetPlatform == TargetPlatform.windows) ...[
-            SizedBox(height: 10.h),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 16.w),
-              child: const AppConfigGameWidget(),
-            )
-          ],
+          SizedBox(height: 10.h),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.w),
+            child: const AppConfigGameWidget(),
+          ),
           SizedBox(height: 10.h),
           Padding(
             padding: EdgeInsets.symmetric(horizontal: 16.w),
